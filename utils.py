f8bb70f79f018093ac0510d446fc7ec8c063b63f
timothydhoe <timothy.dhoe@gmail.com>
2025-10-29 05:21:34 +0100

    helper functions file.

---
+++ utils.py
@@ -0,0 +1,12 @@
+"""
+filename: utils.py
+-------------------------------
+
+This file contains all helper/auxiliary functions for the "Bread Buddy" programme.
+"""
+
+def celsius_to_fahrenheit(C):
+    return (C * 1.8) + 32
+
+def fahrenheit_to_Celsius(F):
+    return (F - 32) * (5/9)
