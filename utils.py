09a4241c555164fca0da1458a10d92bfe24f616c
timothydhoe <timothy.dhoe@gmail.com>
2025-10-29 05:39:16 +0100

    C to F conversions

--- utils.py
+++ utils.py
@@ -6,7 +6,26 @@ This file contains all helper/auxiliary functions for the "Bread Buddy" programm
 """
 
 def celsius_to_fahrenheit(C):
-    return (C * 1.8) + 32
+    return round((C * 1.8) + 32, 1)
 
 def fahrenheit_to_Celsius(F):
-    return (F - 32) * (5/9)
+    return round((F - 32) * (5/9), 1)
+
+
+""" QUICK CONVERSION TEST
+print("Celsius to Fahrenheit")
+print("---------------------")
+print(f"flour:      {celsius_to_fahrenheit(22)}")
+print(f"levain:     {celsius_to_fahrenheit(25)}")
+print(f"ambient:    {celsius_to_fahrenheit(22)}")
+print(f"friction:   {celsius_to_fahrenheit(0)}")
+print(f"ddt:        {celsius_to_fahrenheit(25)}")
+
+print("\nFahrenheit to Celsius")
+print("---------------------")
+print(f"flour:      {fahrenheit_to_Celsius(72)}")
+print(f"levain:     {fahrenheit_to_Celsius(78)}")
+print(f"ambient:    {fahrenheit_to_Celsius(72)}")
+print(f"friction:   {fahrenheit_to_Celsius(32)}")
+print(f"ddt:        {fahrenheit_to_Celsius(78)}")
+"""
